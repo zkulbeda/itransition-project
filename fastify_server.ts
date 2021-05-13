618ac5e6fdeb65f54b817b31fff709eecafa9fff
ZKulbeda <zkulbeda@yandex.ru>
2021-05-14 02:04:43 +0300

    debug: try to change ip address

--- fastify_server.ts
+++ fastify_server.ts
@@ -51,6 +51,6 @@ app.ready(async (err) => {
     mainLogger.fatal(err.message, err);
     return;
   }
-  await app.listen(+process.env.PORT || 3000);
+  await app.listen(+process.env.PORT || 3000, '0.0.0.0');
   console.log('Successful run!', +process.env.PORT || 3000, +process.env.PORT, process.env.PORT);
 });
