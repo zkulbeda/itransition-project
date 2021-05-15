c6011aab0eb205072c39d67664f5425759e7cbf6
ZKulbeda <zkulbeda@yandex.ru>
2021-05-15 16:32:12 +0300

    feat(database): add new entities to database init

--- modules/database.ts
+++ modules/database.ts
@@ -1,6 +1,15 @@
 import { createConnection } from 'typeorm';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import User from '../entity/User';
+import Campaign from '../entity/Campaign';
+import CampaignBonus from '../entity/CampaignBonus';
+import CampaignMatter from '../entity/CampaignMatter';
+import CommentVote from '../entity/CommentVote';
+import Picture from '../entity/Picture';
+import Post from '../entity/Post';
+import Rating from '../entity/Rating';
+import Tag from '../entity/Tag';
+import Comment from '../entity/Comment';
 
 export default function getBDInstance(synchronize: boolean = false, logging: boolean = false) {
   return createConnection({
@@ -12,6 +21,15 @@ export default function getBDInstance(synchronize: boolean = false, logging: boo
     database: process.env.MYSQL_DATABASE,
     entities: [
       User,
+      Campaign,
+      CampaignBonus,
+      CampaignMatter,
+      Comment,
+      CommentVote,
+      Picture,
+      Post,
+      Rating,
+      Tag,
     ],
     synchronize,
     logging,
